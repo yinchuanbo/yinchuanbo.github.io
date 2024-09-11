c4b81af8304cbea8a4028bc6b8b6b0ad5fb921f1
YinHao <1469786443@qq.com>
2024-09-11 17:28:56 +0800

    add

--- articles/html/06 错误监控系统 - download/handleCheckAll.js
+++ articles/html/06 错误监控系统 - download/handleCheckAll.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-const fs2 = require("fs").promises;
 const { minify } = require("terser");
 const path = require("path");
 
@@ -9,17 +8,52 @@ const langs = ["ar", "de", "en", "es", "fr", "it", "jp", "kr", "pt", "tw"];
 
 langs.forEach((lan) => {
   const destinationFilePath = path.join(__dirname, lan, "dist/js/check-all.js");
-  fs.readFile(sourceFilePath, "utf8", async (err, data) => {
-    if (err) {
-      console.error("Error reading the source file:", err);
-      return;
+  const patilesPath = `./${lan}/patiles`;
+  const scriptTag =
+    '<script type="text/javascript" src="/dist/js/check-all.js"></script>\n';
+  function processDirectorySync(dirPath) {
+    try {
+      const files = fs.readdirSync(dirPath);
+      files.forEach((file) => {
+        const filePath = path.join(dirPath, file);
+        const stats = fs.statSync(filePath);
+        if (stats.isDirectory()) {
+          processDirectorySync(filePath);
+        } else if (path.extname(file) === ".ejs" && file.endsWith("head.ejs")) {
+          processFileSync(filePath);
+        }
+      });
+    } catch (err) {
+      console.error("Unable to scan directory:", err);
     }
-    const codeContent = data;
-    const minified = await minify(codeContent);
-    fs.writeFile(destinationFilePath, minified.code, (err) => {
-      if (err) {
+  }
+
+  function processFileSync(filePath) {
+    try {
+      const data = fs.readFileSync(filePath, "utf8");
+      if (data.includes(scriptTag)) {
+        console.log(`Script already exists in: ${filePath}`);
+        return;
+      }
+      const updatedData = scriptTag + data;
+      fs.writeFileSync(filePath, updatedData, "utf8");
+      console.log(`Script added to: ${filePath}`);
+    } catch (err) {
+      console.error("Error reading or writing file:", err);
+    }
+  }
+
+  try {
+    const codeContent = fs.readFileSync(sourceFilePath, "utf8");
+    minify(codeContent).then((minified) => {
+      try {
+        fs.writeFileSync(destinationFilePath, minified.code);
+        processDirectorySync(patilesPath);
+      } catch (err) {
         console.error("Error writing to the destination file:", err);
       }
     });
-  });
+  } catch (err) {
+    console.error("Error reading the source file:", err);
+  }
 });
