651b8f5b587ccb58e2fd2343a1ca7a1623b9a106
20230604-154420\Administrator <yinhaocomeon@gmail.com>
2024-11-06 20:25:27 +0800

    add

--- js/edit2.js
+++ js/edit2.js
@@ -1,7 +1,37 @@
-const navDom = document.querySelector('#nav')
-const switchDom = document.querySelector('.switch')
+const navDom = document.querySelector("#nav");
+const switchDom = document.querySelector(".switch");
 
 switchDom.onclick = () => {
-    console.log('ddd')
-    navDom.classList.toggle('showNav')
-}
+  console.log("ddd");
+  navDom.classList.toggle("showNav");
+};
+
+const liActive = document.querySelector("#nav li.active");
+liActive.scrollIntoView({
+  behavior: "smooth",
+  block: "center",
+  inline: "nearest",
+});
+
+const imgs = document.querySelectorAll("main img");
+imgs.forEach((img) => {
+  img.onclick = () => {
+    img.style.opacity = 0;
+    let imgHove = document.querySelector(".img-hover");
+    if (imgHove) imgHove.remove();
+    const src = img.src;
+    const html = `
+            <div class="img-hover">
+              <img src="${src}" />
+            </div>
+          `;
+    document.body.insertAdjacentHTML("beforeend", html);
+    document.body.style.overflow = "hidden";
+    imgHove = document.querySelector(".img-hover");
+    imgHove.onclick = () => {
+      imgHove.remove();
+      document.body.style.overflow = "initial";
+      img.style.opacity = 1;
+    };
+  };
+});
