75eb59b0f010ea57337710c2e7fda80da45240ae
YinHao <1469786443@qq.com>
2024-11-07 13:12:49 +0800

    add

--- js/edit2.js
+++ js/edit2.js
@@ -2,7 +2,6 @@ const navDom = document.querySelector("#nav");
 const switchDom = document.querySelector(".switch");
 
 switchDom.onclick = () => {
-  console.log("ddd");
   navDom.classList.toggle("showNav");
 };
 
