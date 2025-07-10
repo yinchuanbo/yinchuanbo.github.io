cd5511110893740cc10081b00dcda75712f54ab9
YinHao <1469786443@qq.com>
2025-07-10 17:09:07 +0800

    更新标题为“灏天阁”，删除示例文章文件，调整样式表中的一些样式和注释，优化代码格式。

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const indexTemplate = fs.existsSync(indexTemplatePath)
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>知识库</title>
+      <title>灏天阁</title>
       <link rel="stylesheet" href="/css/style.css">
     </head>
     <body>
